35c5b5db40c8da77656ea396e271af533ad5e04b
alesscpz <alessandro.capozzi@epitech.eu>
2025-04-17 14:50:13 +0200

    feat: add to the libgraphic some modular function that just create, draw and delete a tab of button

--- utils/graphic/libgraphic.h
+++ utils/graphic/libgraphic.h
@@ -10,6 +10,25 @@
 
     #include <SFML/Graphics.h>
 
+typedef struct button_s {
+    sfTexture *texture;
+    sfSprite *sprite;
+    sfIntRect area;
+    sfVector2i pos;
+} button_t;
+
+typedef struct button_tab_s {
+    char *path_sprite;
+    sfIntRect rect;
+    sfVector2i pos;
+} button_tab_t;
+
+    #define SIZE_TAB_BUTTON 4
+
+static const button_tab_t tab_button[] = {
+    {NULL, {0, 0, 0, 0}, {0, 0}}
+};
+
 sfRenderWindow *make_window(sfVector2i dimensions,
     sfVector2i fps_bits, char *name);
 
