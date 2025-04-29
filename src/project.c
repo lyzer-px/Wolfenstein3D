b4bbfaf9e04abf6f78fb1602a7e56b8ede7c3efe
Côme Fougère <come.fougere@epitech.eu>
2025-04-29 14:20:22 +0200

    style: separate create_ and destroy window from project function

--- src/project.c
+++ src/project.c
@@ -14,21 +14,32 @@
 #include "project_funct.h"
 #include "libgraphic.h"
 
+static void create_window(window_t *window)
+{
+    sfVector2i dimensions = {DIM_X, DIM_Y};
+
+    window->clock = sfClock_create();
+    window->window = make_window(dimensions, BITS, NAME_WIN, STYLE_WIND);
+    window->full_screen = false;
+}
+
+static void destroy_window(window_t *window)
+{
+    sfClock_destroy(window->clock);
+    sfRenderWindow_destroy(window->window);
+    free(window);
+}
+
 int project(void)
 {
     window_t *window = malloc(sizeof(window_t));
-    sfVector2i dimensions = {DIM_X, DIM_Y};
     int status = EPI_SUCESS;
 
     if (window == NULL)
         return EPI_FAIL;
     memset(window, 1, sizeof(window_t));
-    window->clock = sfClock_create();
-    window->window = make_window(dimensions, BITS, NAME_WIN, STYLE_WIND);
-    window->full_screen = false;
+    create_window(window);
     status = init_program(window);
-    sfClock_destroy(window->clock);
-    sfRenderWindow_destroy(window->window);
-    free(window);
+    destroy_window(window);
     return status;
 }
