03350593e39708251d5278dcfcb0a8f4ce456215
Côme Fougère <come.fougere@epitech.eu>
2025-04-29 15:05:41 +0200

    fix: replace hard coded value with nb_second variable in update_sprite_rect

--- utils/graphic/update_sprite_rect.c
+++ utils/graphic/update_sprite_rect.c
@@ -25,7 +25,7 @@ void update_sprite_rect(sfSprite *sprite, sprite_rect_t *sprite_rect,
 
     time = sfClock_getElapsedTime(clock);
     seconds = time.microseconds / 1000000.0;
-    if (seconds > 1.0) {
+    if (seconds > nb_seconds) {
         move_rect(sprite_rect);
         sfClock_restart(clock);
     }
