c071a18ac34058e6c4809a5a61e5a3b6d8c0d5f2
alesscpz <alessandro.capozzi@epitech.eu>
2025-05-07 00:03:34 +0200

    style: make the code coding style friendly

--- utils/graphic/type_id.h
+++ utils/graphic/type_id.h
@@ -30,8 +30,7 @@ enum type_id {
     VOID,
 };
 
-struct associative_tab_function_s
-{
+struct associative_tab_function_s {
     void (*setposition)(void *, sfVector2f);
     void (*display)(sfRenderWindow *, void *, sfRenderStates *);
     void (*destroy)(void *);
@@ -39,76 +38,76 @@ struct associative_tab_function_s
 
 static const struct associative_tab_function_s functions_by_type[] = {
     {(void (*)(void *, sfVector2f))sfCircleShape_setPosition,
-    (void (*)(sfRenderWindow *, void *, sfRenderStates *))
+        (void (*)(sfRenderWindow *, void *, sfRenderStates *))
         sfRenderWindow_drawCircleShape,
-    (void (*)(void *))sfCircleShape_destroy},
+        (void (*)(void *))sfCircleShape_destroy},
 
     {(void (*)(void *, sfVector2f))sfConvexShape_setPosition,
-    (void (*)(sfRenderWindow *, void *, sfRenderStates *))
+        (void (*)(sfRenderWindow *, void *, sfRenderStates *))
         sfRenderWindow_drawConvexShape,
-    (void (*)(void *))sfConvexShape_destroy},
+        (void (*)(void *))sfConvexShape_destroy},
 
     {NULL,
-    NULL,
-    (void (*)(void *))sfFont_destroy},
+        NULL,
+        (void (*)(void *))sfFont_destroy},
 
     {NULL,
-    NULL,
-    (void (*)(void *))sfImage_destroy},
+        NULL,
+        (void (*)(void *))sfImage_destroy},
 
     {NULL,
-    NULL,
-    (void (*)(void *))sfShader_destroy},
+        NULL,
+        (void (*)(void *))sfShader_destroy},
 
     {(void (*)(void *, sfVector2f))sfRectangleShape_setPosition,
-    (void (*)(sfRenderWindow *, void *, sfRenderStates *))
+        (void (*)(sfRenderWindow *, void *, sfRenderStates *))
         sfRenderWindow_drawRectangleShape,
-    (void (*)(void *))sfRectangleShape_destroy},
+        (void (*)(void *))sfRectangleShape_destroy},
 
     {NULL,
-    NULL,
-    (void (*)(void *))sfRenderTexture_destroy},
+        NULL,
+        (void (*)(void *))sfRenderTexture_destroy},
 
     {(void (*)(void *, sfVector2f))sfShape_setPosition,
-    (void (*)(sfRenderWindow *, void *, sfRenderStates *))
+        (void (*)(sfRenderWindow *, void *, sfRenderStates *))
         sfRenderWindow_drawShape,
-    (void (*)(void *))sfShape_destroy},
+        (void (*)(void *))sfShape_destroy},
 
     {(void (*)(void *, sfVector2f))sfSprite_setPosition,
-    (void (*)(sfRenderWindow *, void *, sfRenderStates *))
+        (void (*)(sfRenderWindow *, void *, sfRenderStates *))
         sfRenderWindow_drawSprite,
-    (void (*)(void *))sfSprite_destroy},
+        (void (*)(void *))sfSprite_destroy},
 
     {(void (*)(void *, sfVector2f))sfText_setPosition,
-    (void (*)(sfRenderWindow *, void *, sfRenderStates *))
+        (void (*)(sfRenderWindow *, void *, sfRenderStates *))
         sfRenderWindow_drawText,
-    (void (*)(void *))sfText_destroy},
+        (void (*)(void *))sfText_destroy},
 
     {NULL,
-    NULL,
-    (void (*)(void *))sfTexture_destroy},
+        NULL,
+        (void (*)(void *))sfTexture_destroy},
 
     {(void (*)(void *, sfVector2f))sfTransformable_setPosition,
-    NULL,
-    (void (*)(void *))sfTransformable_destroy},
+        NULL,
+        (void (*)(void *))sfTransformable_destroy},
 
     {NULL,
-    (void (*)(sfRenderWindow *, void *, sfRenderStates *))
+        (void (*)(sfRenderWindow *, void *, sfRenderStates *))
         sfRenderWindow_drawVertexArray,
-    (void (*)(void *))sfVertexArray_destroy},
+        (void (*)(void *))sfVertexArray_destroy},
 
     {NULL,
-    (void (*)(sfRenderWindow *, void *, sfRenderStates *))
+        (void (*)(sfRenderWindow *, void *, sfRenderStates *))
         sfRenderWindow_drawVertexBuffer,
-    (void (*)(void *))sfVertexBuffer_destroy},
+        (void (*)(void *))sfVertexBuffer_destroy},
 
     {NULL,
-    NULL,
-    (void (*)(void *))sfView_destroy},
+        NULL,
+        (void (*)(void *))sfView_destroy},
 
     {NULL,
-    NULL,
-    NULL}
+        NULL,
+        NULL}
 };
 
 #endif /* TYPE_ID_H */
