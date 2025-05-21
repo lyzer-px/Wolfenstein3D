da4e625407d922460e7d5518aba0ae00d1967546
lyzer-px <liam.harfouche@epitech.eu>
2025-05-21 21:44:03 +0200

    revert: revert to compilation step

---
+++ assets/main.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** main.c
+** File description:
+** main.c
+*/
+
+#include "macro.h"
+#include "libmy.h"
+#include "project_funct.h"
+
+int main(int argc, char **argv, char **env)
+{
+    argv = (void *)argv;
+    if ((argc == 2 && !my_strcmp("-h", argv[1])) ||
+        (argc == 2 && !my_strcmp("-help", argv[1]))) {
+        print_help();
+        return EPI_SUCCESS;
+    }
+    if (argc != 1)
+        return EPI_FAIL;
+    if (check_env(env) == EPI_FAIL)
+        return EPI_FAIL;
+    return wolf3d();
+}
